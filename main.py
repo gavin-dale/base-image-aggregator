6a37a2dd6c9dc98ee4a332551b6d8ce280ee870c
gdale <gavindale00@gmail.com>
2021-04-19 16:42:36 -0700

    most of main logic exists, for MVP [Cjust need to finish up scraping dockerfile paths from github response, then format the output

--- main.py
+++ main.py
@@ -1,22 +1,75 @@
-# TODO: find Dockerfile by crawling file tree
-# TODO: handle non-200 responses
-# TODO: write to log file and error file
-# TODO: read values from file
-# TODO: curl file from external location first, put in __init__.py?
-# TODO: refactor into classes?
+# TODO:
+# handle non-200 responses
+# write to log file
+# refactor into classes
+# run as k8s job
+# unit testing
+# dockerfile class that imports a request class has project name, repo name, sha
+# request class has base url, url structure, dockerfile paths
+# report class base image name, url, sha, path to dockerfile
+
 import requests
+import json
 
-repo_url = 'https://github.com/app-sre/qontract-reconcile.git'
-repo_url = repo_url.split('/')
+def get_sources():
+    sources = requests.get('https://gist.githubusercontent.com/jmelis/c60e61a893248244dc4fa12b946585c4/raw/25d39f67f2405330a6314cad64fac423a171162c/sources.txt')
+    return sources.text.split('\n')
 
-commit_sha = '30af65af14a2dce962df923446afff24dd8f123e'
-api_base_url = 'https://api.github.com/repos'
-project = repo_url[3]
-repo = repo_url[4][:-4]
+source_dict = {}
+def build_dict(sources_list):
+    for source in sources_list:
+        if source:
+            key_value = source.split(' ')
+            source_dict[key_value[0]] = key_value[1]
+    return source_dict
 
-#target = f'{api_base_url}/{project}/{repo}/contents/dockerfiles?ref={commit_sha}'
-target = f'{api_base_url}/{project}/{repo}/contents/dockerfiles'
+def extract_url_data(source_dict):
+    urls = []
+    for source, sha in source_dict.items(): 
+        repo_url = source.split('/')
+        commit_sha = sha
 
-r = requests.get(target, params=commit_sha)
-print(r.url)
-print(r.json())
+        api_base_url = 'https://api.github.com/repos'
+        project = repo_url[3]
+        repo = repo_url[4][:-4]
+
+        urls.append(f'{api_base_url}/{project}/{repo}/git/trees/{commit_sha}?recursive=true')
+    return urls
+
+def get_trees(urls):
+    trees = []
+    for url in urls:
+        trees.append(requests.get(f'{url}').json())
+    with open('trees.json', 'w') as outfile:
+        json.dump(trees, outfile)
+    return trees
+
+def get_paths(trees):
+    paths = []
+    for tree in trees:
+        print(tree)
+    return(paths)
+
+def get_base_image(url):
+    raw = requests.get(url)
+    base_image = raw.text.split('\n')
+    base_image = base_image[0].replace('FROM', '')
+
+# steps:
+# retrieve sources
+# build dictionary
+# get tree
+# get dockerfile locations
+# get dockerfile contents
+# get base image
+# write report 
+
+sources_list = get_sources()
+
+sources_dict = build_dict(sources_list)
+
+urls = extract_url_data(sources_dict)
+
+trees = get_trees(urls)
+
+paths = get_paths(trees)
