375509821a2e0dd5cc91ed9111b9e2dec9228298
gdale <gavindale00@gmail.com>
2021-04-22 18:01:03 -0700

    adding some comments to explain main.py

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ from report import Report
 import sys
 
 def get_sources(source_url):
+    """Fetches git URLs and commit SHAs from provided URL"""
     sources = requests.get(source_url)
     return sources.text.split('\n')
 
 source_dict = {}
 def build_dict(sources_list):
+    """Builds a dictionary from the list we create from the source URL"""
     for source in sources_list:
         if source:
             key_value = source.split(' ')
@@ -18,6 +20,10 @@ def build_dict(sources_list):
 sources_list = get_sources(sys.argv[1])
 sources_dict = build_dict(sources_list)
 
+"""Loop through the dictionary, creat a new Repository object for each key value pair in the dictionary
+   build out the GitHub URL needed to fetch the recursive tree of directories and files in the repo,
+   gather the Dockerfile paths found in that tree, retrieve the plaintext Dockerfile contents,
+   parse out the name of the base images(s) found within, then write it all to a json file. """
 report_data = []
 for repo_link,commit_sha in sources_dict.items():
     repo = Repository(repo_link,commit_sha)
